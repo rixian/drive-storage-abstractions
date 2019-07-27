11c155792461f28783e1d872d9a3531ed459b2b4
Samuel McAravey <mcaravey@rixian.net>
2019-07-27 07:00:00 -0700

    Added TestCases project.

---
+++ src/Rixian.Drive.Storage.Abstractions.TestCases/TestHarnessBase.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Rixian. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for full license information.
+
+namespace Rixian.Drive.Storage.Abstractions.TestCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.Extensions.DependencyInjection;
+    using NSubstitute;
+    using Rixian.Drive.Storage.Abstractions;
+    using Xunit.Abstractions;
+
+    public abstract class TestHarnessBase : IDisposable
+    {
+        protected const string DefaultStorageDriverInfo = "UseDefaultDriver=true;";
+
+        protected static readonly Guid DefaultDriveControllerId = Guid.Parse("c33f55ed-5e53-40e9-9282-d1c824bd1b54");
+        private readonly Dictionary<string, IServiceProvider> serviceProviders = new Dictionary<string, IServiceProvider>();
+        private bool disposedValue = false; // To detect redundant calls
+
+        public TestHarnessBase(ITestOutputHelper logger)
+        {
+            this.Logger = logger;
+        }
+
+        protected ITestOutputHelper Logger { get; }
+
+        protected List<IDisposable> Disposables { get; } = new List<IDisposable>();
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    foreach (IDisposable disposable in this.Disposables)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+
+                this.disposedValue = true;
+            }
+        }
+    }
+}
