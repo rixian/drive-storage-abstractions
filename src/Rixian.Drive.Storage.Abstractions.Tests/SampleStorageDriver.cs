7c45b2dbfc4681670dc0729378062333c93a63d9
Samuel McAravey <SamuelMcAravey@users.noreply.github.com>
2020-04-16 15:29:21 -0700

    Fixes for sample storage driver.

--- src/Rixian.Drive.Storage.Abstractions.Tests/SampleStorageDriver.cs
+++ src/Rixian.Drive.Storage.Abstractions.Tests/SampleStorageDriver.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -12,15 +14,32 @@ using Xunit.Abstractions;
 
 public class SampleStorageDriver : IStorageDriver
 {
-    public Task DeleteAsync(Guid tenantId, Guid partitionId, Guid fileId, string streamName, string alternateId, CancellationToken cancellationToken = default)
+    public string DriverVersion => "1.0";
+
+    public Task DeleteAsync(DeleteOperationParameters parameters, CancellationToken cancellationToken)
     {
-        File.Delete(this.GetLocalPath(tenantId, partitionId, fileId, streamName));
+        if (parameters is null)
+        {
+            throw new ArgumentNullException(nameof(parameters));
+        }
+
+        var path = this.GetLocalPath(parameters.TenantId, parameters.PartitionId, parameters.FileId, parameters.StreamName);
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+
         return Task.CompletedTask;
     }
 
-    public Task<DriveFile> DownloadAsync(Guid tenantId, Guid partitionId, Guid fileId, string streamName, string alternateId, CancellationToken cancellationToken = default)
+    public Task<DriveFile> DownloadAsync(DownloadOperationParameters parameters, CancellationToken cancellationToken = default)
     {
-        var path = this.GetLocalPath(tenantId, partitionId, fileId, streamName);
+        if (parameters is null)
+        {
+            throw new ArgumentNullException(nameof(parameters));
+        }
+
+        var path = this.GetLocalPath(parameters.TenantId, parameters.PartitionId, parameters.FileId, parameters.StreamName);
         var metadataPath = $"{path}-metadata";
         var fileInfo = new FileInfo(path);
 
@@ -38,24 +57,57 @@ public class SampleStorageDriver : IStorageDriver
         });
     }
 
-    public Task UploadAsync(Guid tenantId, Guid partitionId, Guid fileId, string streamName, string alternateId, Stream stream, DriveFileMetadata fileMetadata = null, CancellationToken cancellationToken = default)
+    public Task<bool> ExistsAsync(ExistsOperationParameters parameters, CancellationToken cancellationToken)
     {
-        if (stream == null)
+        if (parameters is null)
         {
-            throw new ArgumentNullException(nameof(stream));
+            throw new ArgumentNullException(nameof(parameters));
         }
 
-        var path = this.GetLocalPath(tenantId, partitionId, fileId, streamName);
+        var path = this.GetLocalPath(parameters.TenantId, parameters.PartitionId, parameters.FileId, parameters.StreamName);
+        return Task.FromResult(File.Exists(path));
+    }
+
+    public Task<ICollection<string>> ListStreamsAsync(ListStreamsOperationParameters parameters, CancellationToken cancellationToken)
+    {
+        if (parameters is null)
+        {
+            throw new ArgumentNullException(nameof(parameters));
+        }
+
+        var path = this.GetLocalPath(parameters.TenantId, parameters.PartitionId, parameters.FileId, null);
+        var files = Directory.GetFiles(path, "*.*");
+        return Task.FromResult<ICollection<string>>(files);
+    }
+
+    public Task UpgradePartitionAsync(Guid tenantId, Guid volumeId, Guid partitionId, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task UploadAsync(UploadOperationParameters parameters, CancellationToken cancellationToken = default)
+    {
+        if (parameters is null)
+        {
+            throw new ArgumentNullException(nameof(parameters));
+        }
+
+        if (parameters.Data == null)
+        {
+            throw new ArgumentNullException(nameof(parameters));
+        }
+
+        var path = this.GetLocalPath(parameters.TenantId, parameters.PartitionId, parameters.FileId, parameters.StreamName);
         var metadataPath = $"{path}-metadata";
 
         using (var fs = new FileStream(path, FileMode.OpenOrCreate))
         {
-            stream.CopyTo(fs);
+            parameters.Data.CopyTo(fs);
         }
 
-        if (fileMetadata != null)
+        if (parameters.FileMetadata != null)
         {
-            var metadataFileJson = JsonConvert.SerializeObject(fileMetadata);
+            var metadataFileJson = JsonConvert.SerializeObject(parameters.FileMetadata);
             File.WriteAllText($"{path}-metadata", metadataFileJson);
         }
 
@@ -65,6 +117,13 @@ public class SampleStorageDriver : IStorageDriver
     private string GetLocalPath(Guid tenantId, Guid partitionId, Guid fileId, string streamName)
     {
         Directory.CreateDirectory($"{tenantId}/{partitionId}/{fileId}");
-        return $"{tenantId}/{partitionId}/{fileId}/{streamName}";
+        if (string.IsNullOrWhiteSpace(streamName))
+        {
+            return $"{tenantId}/{partitionId}/{fileId}";
+        }
+        else
+        {
+            return $"{tenantId}/{partitionId}/{fileId}/{streamName}";
+        }
     }
 }
